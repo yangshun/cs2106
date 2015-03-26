116a4c3f6111fd5dd4c63f6cb8adbf778df9b7eb
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-03-27 02:25:50 +0800

    [P3] Can create file

--- proj3/file-system.py
+++ proj3/file-system.py
@@ -4,7 +4,26 @@ DEBUG = True
 NUM_BLOCKS_IN_DISK = 64
 NUM_BYTES_IN_BLOCK = 64
 NUM_BYTES_IN_INT = 4
+NUM_DESCRIPTORS_IN_BLOCK = 4
 NUM_BITS_IN_BYTE = 8
+
+def convert_filename_to_int(name):
+  result = 0
+  for char in name:
+    result <<= NUM_BITS_IN_BYTE
+    result |= ord(char) 
+  return result
+
+def convert_int_to_filename(num):
+  filename = ''
+  while num > 0:
+    filename = chr(num & 0xff) + filename
+    num >>= NUM_BITS_IN_BYTE
+  return filename
+
+def print_blocks(blocks):
+  for i in range(NUM_BLOCKS_IN_DISK):
+    print i, ':', blocks[i]
 
 class FSError(Exception):
   def __init__(self, value):
@@ -39,10 +58,10 @@ class Disk(object):
     self.blocks[1][2] = 8
     self.blocks[1][3] = 9
 
-  def read_block(num):
+  def read_block(self, num):
     return self.blocks[num]
 
-  def write_block(num, block):
+  def write_block(self, num, block):
     self.blocks[num] = block
 
 class FileSystem(object):
@@ -51,33 +70,56 @@ class FileSystem(object):
     self.current_disk = None
     self.OFT = {}
 
-  # def convert_filename_to_int(name):
-  #   result = 0
-  #   for char in name:
-  #     result <<= NUM_BITS_IN_BYTE
-  #     result |= ord(char) 
-  #   return result
+  def retrieve_file(self, name):
+    if not self.current_disk:
+      raise FSError('Disk not initialized!')
+    num = convert_filename_to_int(name)
+    for block_num in range(7, 10):
+      block_data = self.current_disk.read_block(block_num)
+      for i in range(len(block_data)/2):
+        if block_data[i*2] == num:
+          return block_data[i*2+1]
+    return -1
 
-  # def convert_int_to_filename(num):
-  #   filename = ''
-  #   while num > 0:
-  #     filename = chr(num & 0xff) + filename
-  #     num >>= NUM_BITS_IN_BYTE
-  #   return filename
+  def find_empty_block(self):
+    bitmap = self.current_disk.read_block(0)
+    for i in range(NUM_BLOCKS_IN_DISK):
+      if bitmap[i] == 0:
+        return i
+    return -1
 
-  # def check_existence_of_file(name):
-  #   if not self.current_disk:
-  #     raise FSError('Disk not initialized!')
-  #   num = convert_filename_to_int(name)
+  def create_file(self, name):
+    fd_index = self.retrieve_file(name)
+    if fd_index > 0:
+      raise FSError('File already exists!')
+    else:
+      descriptor_index = -1
+      for num in range(6):
+        block_num = num + 1
+        block_data = self.current_disk.read_block(block_num)
+        for i in range(NUM_DESCRIPTORS_IN_BLOCK):
+          if block_data[i*NUM_DESCRIPTORS_IN_BLOCK] == -1:
+            descriptor_index = num * NUM_DESCRIPTORS_IN_BLOCK + i
+            # Update file descriptor
+            block_data[i*NUM_DESCRIPTORS_IN_BLOCK] = 0
+            self.current_disk.write_block(block_num, block_data)
+            break
+        if descriptor_index != -1:
+          break
 
-  # def create_file(name):
-  #   if not self.current_disk:
-  #     raise FSError('Disk not initialized!')
-  #   if name not in files:
-  #     files.append(name)
-  #     return name + ' created'
-  #   else:
-  #     raise FSError('File <' + name + '> already exists!')
+      free_directory_found = False
+      for block_num in range(7, 10):
+        block_data = self.current_disk.read_block(block_num)
+        for i in range(len(block_data)/2):
+          if block_data[i*2] == -1:
+            block_data[i*2] = convert_filename_to_int(name)
+            block_data[i*2+1] = descriptor_index
+            self.current_disk.write_block(block_num, block_data)
+            free_directory_found = True
+            break
+        if free_directory_found:
+          break
+      return name + ' created'
 
   # def destroy_file(name):
   #   if name in files:
@@ -102,7 +144,10 @@ class FileSystem(object):
       return 'disk initialized'
 
   def save_disk(self, name):
-    self.disks[name] = self.current_disk
+    if not self.current_disk:
+      raise FSError('No disk has been initialized!')
+    else:
+      self.disks[name] = self.current_disk
     return 'disk saved'
 
 
@@ -111,7 +156,7 @@ def main():
   fs = FileSystem()
 
   commands_mapping = {
-    # 'cr': create_file,
+    'cr': fs.create_file,
     # 'de': destroy_file,
     # 'op': open_file,
     # 'cl': close_file,
